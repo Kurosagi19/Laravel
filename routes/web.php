991629f6d44c5717786f2fa0b7fc42f7c2b8a058
Kurosagi19 <thanhlanqwerty@gmail.com>
2023-06-27 11:42:40 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -26,13 +26,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login', [\App\Http\Controllers\demoController::class, 'login'])->name('login');
 Route::get('/register', [\App\Http\Controllers\demoController::class, 'register'])->name('register');
 
-// Route read (lấy dữ liệu từ database)
-Route::get('/brand', [\App\Http\Controllers\BrandController::class, 'index'])->name('index');
+Route::prefix('/brand')->group(function(){
+    // Route read (lấy dữ liệu từ database)
+    Route::get('/brand', [\App\Http\Controllers\BrandController::class, 'index'])->name('index');
+// Route hiển thị form thêm dữ liệu lên database
+    Route::get('/brand/create', [\App\Http\Controllers\BrandController::class, 'create'])->name('create');
+// Route đẩy dữ liệu lên database
+    Route::post('/brand/create', [\App\Http\Controllers\BrandController::class, 'store'])->name('store');
+// Route lấy dữ liệu từ database về form edit
+    Route::get('/brand/{id}/edit', [\App\Http\Controllers\BrandController::class, 'edit'])->name('edit');
+// Route update dữ liệu lên database
+    Route::put('/brand/{id}/edit', [\App\Http\Controllers\BrandController::class, 'update'])->name('update');
+});
 
-Route::get('/brand/create', [\App\Http\Controllers\BrandController::class, 'create'])->name('create');
+Route::prefix('/products')->group(function(){
 
-Route::post('/brand/create', [\App\Http\Controllers\BrandController::class, 'store'])->name('store');
-
-Route::get('/brand/{id}/edit', [\App\Http\Controllers\BrandController::class, 'edit'])->name('edit');
-
-Route::put('/brand/{id}/edit', [\App\Http\Controllers\BrandController::class, 'update'])->name('update');
+});
